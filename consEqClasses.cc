7b953038a7fc1379217a0654f7b8583a6edb4d63
Frederick Schneider <frederick.schneider2011@gmail.com>
2019-06-10 11:16:02 -0400

    Add in multiplicity to reduce the number of subclass substantially.  Comments / arguments enhanced. Practically only works through 19

--- consEqClasses.cc
+++ consEqClasses.cc
@@ -5,15 +5,20 @@
 #include <map>
 #include <ctime>
 #include <fstream>
+#include <tuple>
+#include <functional>
 #include "sys/time.h"
 
-const bool DEBUG = false; 
+// Counting possible solution sets over consecutive integer ranges
+// See this link for results and explanation: https://docs.google.com/spreadsheets/d/1FQm8LunkZz2oGDZnl_soZCJOVWqv4H_rFMIe-Cyet30/
+
+bool DEBUG;  
+const int FLIP = 2, SYM = 4; // setting bits
 const bool DEEP_DIVE = true;
-using namespace std; using std::string; using std::cout; using std::cin; using std::vector; using std::set; using std::pair;
-using std::unordered_set; 
+using namespace std; 
 
 typedef long UL;
-typedef vector<pair<vector<UL>, pair<bool,bool>>> SubClassTupV; 
+typedef vector<pair<vector<UL>, int>> SubClassTupV; 
 typedef map<vector<UL>, SubClassTupV> SubClassMap; 
 typedef pair<vector<UL>, SubClassTupV> SubClassPair; // This would be a factor footprint plus a list of rows
 typedef vector<vector<SubClassPair>> SubClassVec; 
@@ -31,9 +36,10 @@ struct VectorHash {
 };
 
 typedef unordered_set<vector<UL>, VectorHash> SubClassSet;
-//typedef set<vector<UL>> SubClassSet;
 
 /*
+Notes: 
+
 Enhancements use powers instead of the raw numbers.  
 Instead of 1 3 1 => 0 1 0 at level 3
 Then, we have a smaller unique set of values.
@@ -45,19 +51,16 @@ Would it make sense to allow for the next higher power.
 
 Other steps, combining layers of primes.
 
-What about using an unordered set?  30% performance boost.
+Done: What about using an unordered set?  30% performance boost.
 
-That might be nice with the big integer
+Probably slower: That might be nice with the big integer
 
 The other things to do is to determine the savings collapses for each prime at each level.
 and whether or not it makes sense to do that.  I think it usually would.
 
-If only one entry in the chain.  It would have to be symmetric and you seemingly wouldn't have to multiply by it.
-If you have only one entry in the factor field like 1 1 27 1 1.  Do you actually have to multiply by it?
-(after the first level)
-If there was only one, it would have to be symmetric, right?
-Or should you strip out the symmetric entries and just multiply them first?
-But this is kind of a fringe case.
+No: If only one entry in the chain.  It would have to be symmetric and you seemingly wouldn't have to multiply by it.
+Yes: If you have only one entry in the factor field like 1 1 27 1 1.  Do you actually have to multiply by it? (after the first level)
+No: If there was only one, it would have to be symmetric, right? Or should you strip out the symmetric entries and just multiply them first? But this is kind of a fringe case.
 
 */
 
@@ -93,6 +96,16 @@ bool isFlip(const vector<UL> &v1, const vector<UL> &v2) { // 1 1 3 is a "flip" o
   for (; it1 != v1.end() && it2 != v2.rend(); it1++, it2++) if (*it1 != *it2) return false; 
   return true; 
 }
+
+int getMult(int v) {
+  return (v % 2 + 1); 
+}
+int getFlip (int v) {
+  return ( (v % SYM) / FLIP); 
+}
+int getSym (int v) {
+  return (v / SYM); 
+}
  
 SubClassMap genSubclass(int p, int n) {
   SubClassMap subclasses; 
@@ -100,6 +113,7 @@ SubClassMap genSubclass(int p, int n) {
   int maxPower = p; 
   int tmp = (n-1)/p; 
   while (tmp > 0) maxPower *= p, tmp /= p;
+  int maxPowerM1 = maxPower / p; 
   SubClassSet sv; 
   vector<UL> range(n, 0); 
   if (DEBUG) cout << "For n = " << n << ", p = " << p << " has maxPower = " << maxPower << endl; 
@@ -118,17 +132,37 @@ SubClassMap genSubclass(int p, int n) {
     }
     if (sv.find(range) != sv.end()) continue; // already seen
     sv.insert(range); 
-    // possible enhancement for later: collapse the "extra power" entries and increment the count
+    // possible enhancement for later: collapse the "extra power" entries and increment the count.  Add multiplicity
 
     vector<UL> ff; // we group by factor footprint
-    for (auto pp: range) if (pp > 1) ff.push_back(pp);  
-    sort(ff.begin(), ff.end());
+    int maxCount = 0, maxM1Count = 0; 
     
+    for (auto pp: range) {
+      if (pp > 1) ff.push_back(pp); 
+      if (pp == maxPower) maxCount++; 
+      if (pp == maxPowerM1) maxM1Count++;  
+    }
+    sort(ff.begin(), ff.end());
+
+    // OPTIMIZATION: If there is a row with p^max set once but not p^max-1.  The inverse will be true.  Skip one class and set the
+    // multiplicity flag for the other (so it will be counted twice). 
+    bool multiplicity; 
+    if (maxM1Count + maxCount == 1) { // one or the other is set
+      if (maxM1Count == 1) {
+        if (DEBUG) cout << "Skipping row where " << maxPowerM1 << " is set but " << maxPower << " is not." << endl;
+        continue;
+      } else { 
+        multiplicity = true; // we keep the max count entry
+        if (DEBUG) cout << "Counting row twice where " << maxPowerM1 << " is not set but " << maxPower << " is." << endl;
+      }
+    } else {
+      multiplicity = false; 
+    }
     auto it = subclasses.find(ff); 
     auto symFlag = isSymmetric(range); 
     bool flipFlag = false;  
     if (it == subclasses.end()) {
-      subclasses[ff].push_back(make_pair(range, make_pair(flipFlag, symFlag)));  
+      subclasses[ff].push_back(make_pair(range, multiplicity + FLIP * flipFlag + SYM * symFlag));  
     } else {
       for (auto &v : it->second) {
         if (isFlip(v.first, range)) {
@@ -136,8 +170,8 @@ SubClassMap genSubclass(int p, int n) {
           break; 
         }
       } 
-      // only push the uniq entries if p = 2 (because that's the first prime)
-      if (p !=2 || !flipFlag) it->second.push_back(make_pair(range, make_pair(flipFlag, symFlag))); 
+      // OPTIMIZATION: only push the uniq entries if p = 2 (because that's the first prime)
+      if (p !=2 || !flipFlag) it->second.push_back(make_pair(range, multiplicity + FLIP * flipFlag + SYM * symFlag));
     }
     if (DEBUG) {
       cout << "\tAt index " << i << ", we have: "; for (auto p2 : range) cout << p2 << " "; cout << endl; 
@@ -148,25 +182,34 @@ SubClassMap genSubclass(int p, int n) {
     cout << "Subclasses was blank!" << endl;
     exit(1); 
   }
-  if (DEBUG) {
+  if (DEBUG) {  //Note: The flip flag is never set for 2 because it is the first entry.  Flips are eliminated
     for (auto &pr : subclasses) {
       cout << "Processing ff = "; for (auto &pp : pr.first) cout << pp << " "; cout << ": " << endl;
       for (auto &v : pr.second) {
         cout << "\t"; for (auto &pp : v.first) cout << pp << " "; 
-        cout << "(Flip? " << v.second.first << ", Sym? " << v.second.second << ")" << endl;
+        cout << "(Flip? " << getFlip(v.second) << ", Sym? " << getSym(v.second) << ", Mult: " << getMult(v.second) << ")" << endl;
       }
     }
   }
   return (subclasses);
 }
 
-UL processSubClass(SubClassSet &scSet, const SubClassVec &scVec, const vector<int> &indexes, const vector<UL> &wv, int lvl, bool sym, int primeLenMatch, UL &missCt) {
+UL processSubClass(SubClassSet &scSet, const SubClassVec &scVec, const vector<int> &indexes, 
+                   const vector<UL> &wv, int lvl, bool sym, int primeLenMatch, UL &missCt, int mult) {
   UL ct = 0; 
   if (DEBUG) cout << "Processing lvl = " << lvl << " and " << indexes[lvl] << endl; 
+
   for (auto &pr : scVec[lvl][indexes[lvl]].second) { // correspond set of rows for a given factor footprint
-    if (sym && pr.second.first) continue; // we are symmetric but encountered a flipped entry ... Skip!
-    bool newSym = sym && pr.second.second; // check if still symmetric
-    if (DEBUG) {cout << endl << "____ " << indexes[lvl]; for (int b = 0; b <= lvl; b++) cout << "\t"; for (auto pp : pr.first) cout << pp << " "; cout << endl << endl; }
+    if (sym && getFlip(pr.second)) continue; // we are symmetric but encountered a flipped entry ... Skip!  OPTIMIZATION
+    int newMult = mult * getMult(pr.second); 
+    bool newSym = sym && getSym(pr.second); // check if still symmetric
+
+    if (DEBUG) {
+      cout << endl << "____ " << indexes[lvl]; 
+      for (int b = 0; b <= lvl; b++) cout << "\t"; 
+      for (auto pp : pr.first) cout << pp << " "; 
+      cout << endl << endl; 
+    }
     auto newWV(pr.first); 
     if (lvl > 0) for (int i = 0; i < wv.size(); i++) newWV[i] *= wv[i]; // multiply each entry by the old wv
     if (DEBUG) { cout << "\tProcessing wv = "; for (auto v : newWV) cout << v << " "; cout << endl; }
@@ -183,17 +226,18 @@ UL processSubClass(SubClassSet &scSet, const SubClassVec &scVec, const vector<in
             if (lastVal != v) lastCt++; 
             lastVal = v; 
           }
-          if (DEBUG) { cout << "Added " << lastCt << " at end: wv = "; for (auto v : newWV) cout << v << " "; cout << endl; }
-          ct += lastCt;  
+          if (DEBUG) { cout << "Added " << lastCt << " at end: wv = "; for (auto v : newWV) cout << v << " "; 
+                       cout << ". Mult = " << newMult << "." << endl; }
+          ct += lastCt * newMult;  
         } else {
-          if (DEBUG) { cout << "Added: wv = "; for (auto v : newWV) cout << v << " "; cout << endl; }          
-          ct++; 
+          if (DEBUG) { cout << "Added: wv = "; for (auto v : newWV) cout << v << " "; cout << ". Mult = " << newMult << "." << endl; }          
+          ct += newMult; 
         }
       } else {
         missCt++; 
       } 
     } else {
-      ct += processSubClass(scSet, scVec, indexes, newWV, newLvl, newSym, primeLenMatch, missCt); 
+      ct += processSubClass(scSet, scVec, indexes, newWV, newLvl, newSym, primeLenMatch, missCt, newMult); 
     }
   }
   return ct; 
@@ -218,16 +262,23 @@ int main(int argc, const char *argv[])
 {
   // list of primes
   vector<int> primes{2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97};
-  const int UB = 100; // must be in sync with primes above
-  int sv = 3;
+  const int LB = 3, UB = 100; // must be in sync with primes above
+  int sv = LB, ev = UB;
   if (argc > 1) sv = atoi(argv[1]);
-  if (sv > UB) {
-    cout << "The search param n must be <= " << UB << endl; 
+  if (argc > 2) ev = atoi(argv[2]); 
+  if (sv < LB || sv > UB) {
+    cout << "The search param sv (#1) must be >= " << LB << " and <= " << UB << endl; 
     exit(1); 
   }
+  if (ev > UB || ev < sv ) {
+    cout << "The search param ev (#2 must be <= " << sv << " and <= " << UB << endl;
+    exit(1);
+  }
+  DEBUG = argc > 3 ? atoi(argv[3]) : false;
+  string fName = argc > 4 ? argv[4] : "consSubCounts.txt"; 
+
   ofstream subCountFile;
   if (DEEP_DIVE) {
-    string fName = "consSubCounts.txt";
     backupFile(fName); 
     subCountFile.open(fName); // restart it
   } 
@@ -235,15 +286,15 @@ int main(int argc, const char *argv[])
   SubClassSet scSet;
   if (DEEP_DIVE) scSet.reserve(10000000); // revisit when going beyond 20
 
-  cout << "Running from n = " << sv << " to " << UB << endl;
-  for (int n = sv; n <= UB; n++) {
+  cout << "Running from n = " << sv << " to " << ev << endl;
+  for (int n = sv; n <= ev; n++) {
     // cycle through the primes
     int primeLenMatch = 0;
     SubClassVec scVec; 
     for (auto p: primes) {
       if (p > n) continue;
       if (p == n) {
-        // add an optimization when n is prime: to add each in by the number of distinct values
+        // OPTIMIZATION: when n is prime, add in the number of distinct values
         
         // what about when you have an entry that has all unique values
         // then you can multiply it by anything and it will still be unique
@@ -261,20 +312,24 @@ int main(int argc, const char *argv[])
     UL prod = 1; 
     for (auto &ff : scVec) prod *= ff.size();
 
-    cout << "Here is prod = " << prod << " at n = " << n << endl; 
+    cout << "Here is the subproblem count = " << prod << " at n = " << n << endl; 
 
     vector<int> index; 
     vector<UL> dummy; // empty vector to get things started
-    UL totalCt = 0; 
+    UL totalCt = 0;
     for (UL x = 0; x < prod; x++) { // now we iterate through all of the combinations
       // we must iterate through the primes in the list.  If it's on a prime, then there's a final step
       // of counting all of the combinations (based on unique values). 
       // you also have to keep track of which items are symmetric going in.
+      UL totalTries = 1; 
       vector<int> indexes;
       auto tmp = x; 
       for (int y = 0; y < scVec.size(); y++) {
         auto s = scVec[y].size(); 
-        indexes.push_back(tmp % s);
+        auto m = tmp % s; 
+        indexes.push_back(m);
+
+        totalTries *= scVec[y][m].second.size(); 
         if (DEBUG) cout << "For x = " << x << ", we have " << tmp %s << " for " << s << " at index = " << y << endl;  
         tmp /= s; 
       }
@@ -282,10 +337,11 @@ int main(int argc, const char *argv[])
       if (DEEP_DIVE) {
         scSet.clear();
         UL missCt = 0; 
-        UL subCt = processSubClass(scSet, scVec, indexes, dummy, 0, true, primeLenMatch, missCt);  // XXX pass in the prime flag.  Make the primes global?
+        if (DEBUG) cout << "At: " << x << ", the total tries will be " << totalTries << endl; 
+        UL subCt = processSubClass(scSet, scVec, indexes, dummy, 0, true, primeLenMatch, missCt, 1); // XXX pass in prime flag.  Make the primes global?
         if (DEBUG) cout << "\tThe subcount for x = " << x << " of " << prod -1 << " was " << subCt << " vs. " << scSet.size() 
                         << ".  There were " << missCt << " misses.  (at " << timestamp() << ")" << endl; 
-        subCountFile << n << " " << x << " " << scSet.size() << endl;   
+        subCountFile << n << " " << x << " " << scSet.size() << " " << subCt << " " << timestamp() << endl;   
         totalCt += subCt; 
       }
     }
